3b41a6439623f6994cb264bbb438edd828db7c29
Remy Jourde <remy.jourde@gmail.com>
2015-04-21 15:55:37 +0200

    use PORT env variable

--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-  "html/template"
-  "net/http"
+	"html/template"
+	"net/http"
+	"os"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-  http.HandleFunc("/", index)
+	http.HandleFunc("/", index)
 
-  err := http.ListenAndServe(":8080", nil)
-  if err != nil {
-    panic(err)
-  }
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-  t, _ := template.ParseFiles("templates/index.html")
-  t.Execute(res, nil)
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(res, nil)
 }
